c1de4aaccdee57f833759cf47cec7434cf4d7132
rat00lis <ob.alarcon2001@gmail.com>
2024-06-15 20:26:04 -0400

    se agregó comentarios funciones: get_score, get_matrix, game_over, is_legal

--- matrix.py
+++ matrix.py
@@ -2,21 +2,28 @@ import copy
 import random
 
 class matrix_2048:
+    #constructor
     def __init__ (self):
         self.matrix = [[0] * 4 for _ in range(4)]
         self.add_number()
         self.add_number()
     
+    #añadir un número aleatorio en una celda vacía
     def add_number(self):
         empty = []
-        for i in range(4):
+        for i in range(4): #recorrer la matriz
             for j in range(4):
-                if self.matrix[i][j] == 0:
-                    empty.append((i, j))
-        random_cell = random.choice(empty)
-        random_number = random.randint(0,1)
-        self.matrix[random_cell[0]][random_cell[1]] = 2 if random_number == 0 else 4
+                if self.matrix[i][j] == 0: #si la celda está vacía
+                    empty.append((i, j)) #añadir la celda a la lista de celdas vacías
 
+        if not empty: #si no queda espacio, se acabo el juego
+            return
+        
+        random_cell = random.choice(empty) #se elige una celda aleatoria de la lista de celdas vacías
+        random_number = random.randint(0,1) #se elige un número aleatorio entre 0 y 1
+        self.matrix[random_cell[0]][random_cell[1]] = 2 if random_number == 0 else 4 #se añade un 2 o un 4 a la celda aleatoria
+
+    #mover la matriz hacia arriba
     def up(self):
         old_matrix = copy.deepcopy(self.matrix)
         for j in range(4):
@@ -34,6 +41,7 @@ class matrix_2048:
 
         return old_matrix != self.matrix
     
+    #mover la matriz hacia abajo
     def down(self):
         old_matrix = copy.deepcopy(self.matrix)
         for j in range(4):
@@ -51,6 +59,7 @@ class matrix_2048:
     
         return old_matrix != self.matrix
     
+    #mover la matriz hacia la izquierda
     def left(self):
         old_matrix = copy.deepcopy(self.matrix)
         for i in range(4):
@@ -67,6 +76,7 @@ class matrix_2048:
     
         return old_matrix != self.matrix
     
+    #mover la matriz hacia la derecha
     def right(self):
         old_matrix = copy.deepcopy(self.matrix)
         for i in range(4):
@@ -82,6 +92,39 @@ class matrix_2048:
             self.matrix[i] = row
     
         return old_matrix != self.matrix
+    
+    #el agente de inteligencia artificial debe ser capaz de obtener el estado actual del juego
+    def get_matrix(self):
+        #retornar la matriz
+        return self.matrix
+    
+    #el agente debe conocer el puntaje actual del juego
+    #que se calcula como el mayor número en la matriz
+    def get_score(self):
+        return max([max(row) for row in self.matrix])
+    
+    #el agente debe ser capaz de saber si el juego ha terminado
+    def game_over(self):
+        if self.get_score() == 2048:
+            return True
+        for i in range(4):
+            for j in range(4):
+                if self.matrix[i][j] == 0:
+                    return False
+                if i < 3 and self.matrix[i][j] == self.matrix[i+1][j]:
+                    return False
+                if j < 3 and self.matrix[i][j] == self.matrix[i][j+1]:
+                    return False
+        return True
+
+    #checar si el movimiento es legal, se pasa
+    #el movimiento y se retorna bool
+    def is_legal(self, movement):
+        old_matrix = copy.deepcopy(self.matrix)
+        movement()
+        legal = old_matrix != self.matrix
+        self.matrix = old_matrix
+        return legal
     
     def print_matrix(self):
         for row in self.matrix:
