869c3c013a174bbc59c46f81659c0783ad140ca6
Bruno Arce R. <brunoarce0@gmail.com>
2024-07-04 12:29:37 -0400

    Fixed

---
+++ 2048-game.py
@@ -0,0 +1,179 @@
+import random
+import pygame
+import copy
+import math
+from matrix import matrix_2048
+
+def get_font_settings(cell):
+    settings = {
+        'background_color': (0, 0, 0),
+        'font_color': (255, 255, 255),
+        'font_size': 30,
+    }
+    if cell == 0:
+        settings['background_color'] = '#cdc1b4'
+        settings['font_color'] = '#776e65'
+        settings['font_size'] = 30
+    if cell == 2:
+        settings['background_color'] = '#eee4da'
+        settings['font_color'] = '#776e65'
+        settings['font_size'] = 60
+    elif cell == 4:
+        settings['background_color'] = '#ede0c8'
+        settings['font_color'] = '#776e65'
+        settings['font_size'] = 60
+    elif cell == 8:
+        settings['background_color'] = '#f2b179'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 60
+    elif cell == 16:
+        settings['background_color'] = '#f59563'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 60
+    elif cell == 32:
+        settings['background_color'] = '#f67c5f'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 60
+    elif cell == 64:
+        settings['background_color'] = '#f65e3b'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 60
+    elif cell == 128:
+        settings['background_color'] = '#edcf72'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 50
+    elif cell == 256:
+        settings['background_color'] = '#edcc61'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 50
+    elif cell == 512:
+        settings['background_color'] = '#edc850'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 50
+    elif cell == 1024:
+        settings['background_color'] = '#edc53f'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 40
+    elif cell == 2048:
+        settings['background_color'] = '#edc22e'
+        settings['font_color'] = '#f9f6f2'
+        settings['font_size'] = 40
+    
+    return settings
+
+def draw_game_board(screen, matrix, num_rows, num_cols, matrixStartX, matrixStartY, horizontal, vertical, margin, cellSize):
+    # Fondo de matriz
+    pygame.draw.rect(screen, '#bbada0', (matrixStartX, matrixStartY, horizontal, vertical), border_radius=5)
+
+    # Celdas matriz
+    for i in range(num_rows):
+        for j in range(num_cols):
+            cell = matrix[i][j]
+            cell_settings = get_font_settings(cell)
+            fontNumbers = pygame.font.Font('fonts/ClearSans-Bold.ttf', cell_settings['font_size'])
+            cell_x = matrixStartX + (j + 1) * margin + j * cellSize
+            cell_y = matrixStartY + (i + 1) * margin + i * cellSize
+            pygame.draw.rect(screen, cell_settings['background_color'], (cell_x, cell_y, cellSize, cellSize), border_radius=5)
+            text = fontNumbers.render(str(cell) if cell != 0 else "", True, cell_settings['font_color'])
+            text_rect = text.get_rect(center=(cell_x + cellSize // 2, cell_y + cellSize // 2))
+            screen.blit(text, text_rect)
+
+def draw_game_over(screen, game, WIDTH, HEIGHT):
+    font = pygame.font.Font('fonts/ClearSans-Bold.ttf', 30)
+    finish_text = 'Game over' if not game.game_over() else 'You win'
+    text = font.render(finish_text, True, primary_font_color)
+    text_rect = text.get_rect(center=(WIDTH//2, HEIGHT//2 - 60))
+    screen.blit(text, text_rect)
+
+    text = font.render('Score: ' + str(game.get_score()), True,(primary_font_color))
+    text_rect = text.get_rect(center=(WIDTH//2, HEIGHT//2-20))
+    screen.blit(text, text_rect)
+
+    text = font.render('Press space to restart', True, primary_font_color)
+    text_rect = text.get_rect(center=(WIDTH//2, HEIGHT//2 + 60))
+    screen.blit(text, text_rect)
+
+    pygame.display.flip() 
+
+def draw_game_layout(screen, game, WIDTH, HEIGHT):
+    # Texto 2048
+    font2048 = pygame.font.Font('fonts/ClearSans-Bold.ttf', 50)
+    text2048 = font2048.render('2048', True, primary_font_color)
+    text2048_rect = text2048.get_rect(topleft=(matrixStartX, matrixStartY - score_height // 2 - margin*4))
+    screen.blit(text2048, text2048_rect)
+
+    # Score
+    fontScore = pygame.font.Font('fonts/ClearSans-Bold.ttf', 25)
+    textScore = fontScore.render('Score: ' + str(game.get_score()), True, (255,255,255))
+    textScore_rect = textScore.get_rect(topleft=(matrixStartX + horizontal - textScore.get_width(), matrixStartY - score_height // 2 - margin))
+    score_position_x = matrixStartX + horizontal - textScore.get_width() - margin
+    score_position_y = matrixStartY - score_height // 2 - margin * 2  
+    textScore_rect.topleft = (score_position_x, score_position_y)
+    pygame.draw.rect(screen, '#bbada0', (textScore_rect.x - 10, textScore_rect.y - 5, textScore_rect.width + 20, textScore_rect.height + 10), border_radius=5)
+    screen.blit(textScore, textScore_rect)
+
+game = matrix_2048()
+
+# Window setting
+WIDTH = 700
+HEIGHT = 600
+
+animation_duration = 500
+
+# Matrix setting
+num_rows = len(game.matrix)
+num_cols = len(game.matrix[0])
+margin = 10
+total_margin = (num_cols + 1) * margin
+score_height = 50  # Espacio para el puntaje
+cellSize = (min((WIDTH - margin * 2 - total_margin) // num_cols, (HEIGHT - margin * 2 - total_margin - score_height) // num_rows))
+horizontal = cellSize * num_cols + (num_cols + 1) * margin
+vertical = cellSize * num_rows + (num_rows + 1) * margin
+matrixStartX = WIDTH // 2 - horizontal // 2
+matrixStartY = HEIGHT // 2 - vertical // 2 + score_height // 2  # Ajustar para dejar espacio para el puntaje
+
+# Initialize font
+pygame.font.init()
+font = pygame.font.Font('fonts/ClearSans-Bold.ttf', 30)
+primary_font_color = '#776e65'
+
+# Initialize game
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('2048 Game')
+
+running = True
+while running:
+    background_color = '#faf8ef'
+    screen.fill(background_color)
+
+    # Verificar si el juego ha terminado
+    if game.game_over() != None:
+        draw_game_over(screen, game, WIDTH, HEIGHT)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game.restart()
+        continue
+    
+    draw_game_layout(screen, game, WIDTH, HEIGHT)
+    draw_game_board(screen, game.matrix, num_rows, num_cols, matrixStartX, matrixStartY, horizontal, vertical, margin, cellSize)
+
+    # Eventos
+    valid_movement = False
+    previous_matrix = copy.deepcopy(game.matrix)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            key = pygame.key.name(event.key)
+            if key == 'w' or key == 'up': valid_movement = game.up()
+            elif key == 's' or key == 'down': valid_movement = game.down()
+            elif key == 'a' or key == 'left': valid_movement = game.left()
+            elif key == 'd' or key == 'right': valid_movement = game.right()
+    if valid_movement:
+        game.add_number()
+    pygame.display.flip()
+pygame.quit()
---
+++ matrix.py
@@ -0,0 +1,162 @@
+import copy
+import random
+
+class matrix_2048:
+    #constructor
+    def __init__ (self):
+        self.WINCONDITION = 2048
+        self.score = 0
+        self.matrix = [[0] * 4 for _ in range(4)]
+        self.add_number()
+        self.add_number()
+    
+    #añadir un número aleatorio en una celda vacía
+    def add_number(self):
+        empty = []
+        for i in range(4): #recorrer la matriz
+            for j in range(4):
+                if self.matrix[i][j] == 0: #si la celda está vacía
+                    empty.append((i, j)) #añadir la celda a la lista de celdas vacías
+
+        if not empty: #si no queda espacio, se acabo el juego
+            return
+        
+        random_cell = random.choice(empty) #se elige una celda aleatoria de la lista de celdas vacías
+        random_number = random.randint(0,9) #se elige un número aleatorio entre 0 y 1
+        self.matrix[random_cell[0]][random_cell[1]] = 2 if random_number <= 8 else 4 #se añade un 2 o un 4 a la celda aleatoria
+
+    # mover la matriz hacia arriba
+    def up(self):
+        old_matrix = copy.deepcopy(self.matrix)
+        for j in range(4):
+            column = [self.matrix[i][j] for i in range(4)]
+            column = [x for x in column if x != 0]
+            column += [0] * (4 - len(column))
+            for i in range(3):
+                if column[i] == column[i+1]:
+                    column[i] *= 2
+                    column[i+1] = 0
+                    self.score += column[i]
+            column = [x for x in column if x != 0]
+            column += [0] * (4 - len(column))
+            for i in range(4):
+                self.matrix[i][j] = column[i]
+        return old_matrix != self.matrix
+    
+    #mover la matriz hacia abajo
+    def down(self):
+        old_matrix = copy.deepcopy(self.matrix)
+        for j in range(4):
+            column = [self.matrix[i][j] for i in range(4)]
+            column = [x for x in column if x != 0]
+            column = [0] * (4 - len(column)) + column
+            for i in range(3, 0, -1):
+                if column[i] == column[i-1]:
+                    column[i] *= 2
+                    column[i-1] = 0
+                    self.score += column[i]
+            column = [x for x in column if x != 0]
+            column = [0] * (4 - len(column)) + column
+            for i in range(4):
+                self.matrix[i][j] = column[i]
+    
+        return old_matrix != self.matrix
+    
+    #mover la matriz hacia la izquierda
+    def left(self):
+        old_matrix = copy.deepcopy(self.matrix)
+        for i in range(4):
+            row = self.matrix[i]
+            row = [x for x in row if x != 0]
+            row += [0] * (4 - len(row))
+            for j in range(3):
+                if row[j] == row[j+1]:
+                    row[j] *= 2
+                    row[j+1] = 0
+                    self.score += row[j]
+            row = [x for x in row if x != 0]
+            row += [0] * (4 - len(row))
+            self.matrix[i] = row
+    
+        return old_matrix != self.matrix
+    
+    #mover la matriz hacia la derecha
+    def right(self):
+        old_matrix = copy.deepcopy(self.matrix)
+        for i in range(4):
+            row = self.matrix[i]
+            row = [x for x in row if x != 0]
+            row = [0] * (4 - len(row)) + row
+            for j in range(3, 0, -1):
+                if row[j] == row[j-1]:
+                    row[j] *= 2
+                    row[j-1] = 0
+                    self.score += row[j]
+            row = [x for x in row if x != 0]
+            row = [0] * (4 - len(row)) + row
+            self.matrix[i] = row
+        
+        return old_matrix != self.matrix
+    
+    #el agente de inteligencia artificial debe ser capaz de obtener el estado actual del juego
+    def get_state(self):
+        #retornar la matriz
+        return self.matrix
+    
+    #el agente debe conocer el puntaje actual del juego
+    #que se calcula como el mayor número en la matriz
+    def get_max_value(self):
+        return max([max(row) for row in self.matrix])
+    
+    def get_score(self):
+        return self.score
+    
+    #suma de toda la matriz
+    def sum_matrix(self):
+        return sum([sum(row) for row in self.matrix])
+    
+    # el agente debe ser capaz de saber si el juego ha terminado
+    # True si ha ganado, False si ha perdido, None si no ha terminado
+    def game_over(self):
+        if self.get_max_value() >= self.WINCONDITION:
+            return True
+        for i in range(4):
+            for j in range(4):
+                if self.matrix[i][j] == 0:
+                    return None
+                if i < 3 and self.matrix[i][j] == self.matrix[i+1][j]:
+                    return None
+                if j < 3 and self.matrix[i][j] == self.matrix[i][j+1]:
+                    return None
+        return False
+    
+    def print_matrix(self):
+        for row in self.matrix:
+            print(row)
+    
+    #se puede probar un movimiento sin cambiar la matriz
+    #y se retorna la matriz resultante
+    def try_step(self, move):
+        new_matrix = copy.deepcopy(self.matrix)
+        if move == 'up':
+            self.up()
+        elif move == 'down':
+            self.down()
+        elif move == 'left':
+            self.left()
+        elif move == 'right':
+            self.right()
+        else:
+            #exception
+            return None
+        temp = copy.deepcopy(self.matrix)
+        self.matrix = new_matrix
+        return temp
+    
+    #resetear
+    def restart(self):
+        self.score = 0
+        self.matrix = [[0] * 4 for _ in range(4)]
+        self.add_number()
+        self.add_number()
+    
